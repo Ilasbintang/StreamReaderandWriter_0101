61bd3453d708432399992e569346fa0eaae99bef
Ilasbintang <ilasbintang727@gmail.com>
2025-06-10 15:28:54 +0700

    commit 10

---
+++ iofileWithPath.cpp
@@ -0,0 +1,21 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string baris;
+    string NamaFile;
+
+    cout << "Masukan Nama File : ";
+    cin >> NamaFile;
+
+    outfile.open(NamaFile + ".txt", ios::out);
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+    while (true) {
+        cout << "- ";
+        getline(cin, baris);
+        if (baris == "q") break;
+        outfile << baris << endl;
+    }
+}
