6550b38d40de3cb980898a9c7b0da9568da22c6c
Ilasbintang <ilasbintang727@gmail.com>
2025-06-10 14:52:52 +0700

    commit 2

--- iofile.cpp
+++ iofile.cpp
@@ -12,4 +12,18 @@ int main() {
     outfile.open("contohfile.txt");
 
     cout << ">=Menulis file, \'q\' untuk keluar" << endl;
+    
+    //unlimeted loop untuk menulis
+    while (true) {
+        cout << "- ";
+        //mendapatkan setiap karakter dalam satu baris
+        getline(cin, baris);
+        //loop akan berhenti ketika anda memasukan karakter q
+        if (baris == "q") break;
+        //menulis dan memasukan nilai dari 'baris' ke dalam file
+        outfile << baris << endl;
+    }
+    //selesai dalam menulis sekrang tutup filenya
+    outfile.close();
+    //membuka file dalam mode membaca
 }
